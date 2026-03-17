fa2fa17e7007ee5f2b3dc3025a0245435d0dd5ae
malyshkoserhii <malyshkosergiy@gmail.com>
2026-03-17 16:13:58 +0200

    feat: Added endpoint for retrieving auth user

--- routes/api.php
+++ routes/api.php
@@ -4,13 +4,18 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth:sanctum');
 
+Route::middleware('auth:sanctum',)->group(function () {
+    Route::get('/me', [UserController::class, 'me'])->name('user.me');
+});
 
-Route::middleware('auth:sanctum', 'role:admin|user')->group(function () {
+
+Route::middleware('auth:sanctum',)->group(function () {
     Route::get('/todos', [TodoController::class, 'index'])->name('todos.index');
     Route::post('/todos', [TodoController::class, 'store'])->name('todos.store');
     Route::get('/todos/{id}', [TodoController::class, 'show'])->name('todos.show');
@@ -18,7 +23,7 @@ Route::middleware('auth:sanctum', 'role:admin|user')->group(function () {
     Route::delete('/todos/{id}', [TodoController::class, 'destroy'])->name('todos.destroy');
 });
 
-Route::middleware('auth:sanctum', 'role:admin|user')->group(function () {
+Route::middleware('auth:sanctum')->group(function () {
     Route::get('/comments/{todoId}', [CommentController::class, 'getCommentsBytodoId'])->name('comments.getCommentsBytodoId');
     Route::post('/comments', [CommentController::class, 'store'])->name('comments.store');
     Route::patch('/comments/{id}', [CommentController::class, 'update'])->name('comments.update');
